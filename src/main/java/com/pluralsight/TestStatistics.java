2f67ca890388658276aa81e8500356de8e959fe3
BogdanATA <batameev@gmail.com>
2026-04-16 15:13:09 -0400

    - created logic to get the average test score

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
 public class TestStatistics {
+    public static void main(String[] args) {
+
+        //array of 10 test scores
+        int testScores[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+        //average test score
+        double sum = 0;
+
+        for (int i = 0; i < testScores.length; i++) {
+            sum += testScores[i];
+
+        }
+        double average = sum / testScores.length;
+        System.out.println("The average test score is: " + average);
+
+
+
+
+    }
 }
